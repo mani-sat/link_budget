63456a2331a3195fd889bb4439901325e45121c2
Rasmus <rdybkj21@student.aau.dk>
2025-05-09 10:25:49 +0200

    Updated to use running mean model

--- mani_rain_package/mani_rain/rain/__init__.py
+++ mani_rain_package/mani_rain/rain/__init__.py
@@ -8,4 +8,10 @@ nn_model = markov_base.from_file(
     f"{__path__[0]}/models/nn_model.npy",
     f"{__path__[0]}/models/nn_states.npy"
 )
+aau_ma_model = markov_base.from_file(
+    f"{__path__[0]}/models/aau_ma_model.npy",
+    f"{__path__[0]}/models/aau_ma_states.npy"
+)
+
+
 
