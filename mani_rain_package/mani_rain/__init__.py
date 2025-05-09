8ed35771a60aadb94607324fafefff8eea7321b3
Rasmus <rdybkj21@student.aau.dk>
2025-05-09 11:03:04 +0200

    Added Aalborg station to init

--- mani_rain_package/mani_rain/__init__.py
+++ mani_rain_package/mani_rain/__init__.py
@@ -1,4 +1,4 @@
-from ._core import cebreros, malargue, new_norcia, station_t, mani_link 
+from ._core import cebreros, malargue, new_norcia, aalborg, station_t, mani_link 
 from .rain.itu import rain_itu
 from .rain import markov_rain
 from .linkbudget import link_budget_itu, link_budget_markov
